0b6b632b09817680c056f1604b50b6cc30775bb9
rajkumar <rajkumar.gootam@gmail.com>
2016-10-02 23:37:47 +0530

    created (nav,register,sucess pages) and created routes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,24 @@
-var myApp = angular.module('myApp',[]);
-myApp.controller('appController',['$scope',function($scope){
-  $scope.message = "Welcome to my App";
-}])
+var myApp = angular.module('myApp',['ngRoute']);
+
+myApp.config(['$routeProvider', function($routeProvider){
+  $routeProvider
+  .when('/login',{
+    templateUrl:'views/login.html',
+    controller:'LoginController'
+  })
+  .when('/registaration',{
+    templateUrl:'views/registration.html',
+    controller:'RegistrationController'
+  })
+  .when('/sucess',{
+    templateUrl:'views/sucess.html',
+    controller:'SucessController'
+  })
+  .otherwise({
+    redirectTo: '/login'
+  });
+}]);
+
+// myApp.controller('appController',['$scope',function($scope){
+//   $scope.message = "Welcome to my App";
+// }])
